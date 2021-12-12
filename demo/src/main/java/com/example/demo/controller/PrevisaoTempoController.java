ea9f4230aa447e5bd179c9d830f26b73068e647b
FirmoGuilherme <firmoguilhevieira2015@gmail.com>
2021-12-12 19:18:59 -0300

    Desafio workDB

---
+++ demo/src/main/java/com/example/demo/controller/PrevisaoTempoController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.PrevisaoTempo;
+import com.example.demo.repository.PrevisaoTempoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+@RestController
+public class PrevisaoTempoController {
+
+    @Autowired
+    private PrevisaoTempoRepository repository;
+
+    @GetMapping(path = "/previsao")
+    public List<PrevisaoTempo> listarTudo() {
+        return repository.findAll();
+    }
+
+    @PostMapping(path = "/previsao")
+    public PrevisaoTempo salvar(@RequestBody PrevisaoTempo previsaoTempo) {
+        Date date=java.util.Calendar.getInstance().getTime();
+        previsaoTempo.setDia(date);
+        return repository.save(previsaoTempo);
+    }
+}
